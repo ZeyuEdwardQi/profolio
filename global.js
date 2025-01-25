2872a21cb3fb2bd54690500b96aeba4ca270ef0f
ZeyuEdwardQi <zeqi@ucsd.edu>
2025-01-24 16:16:23 -0800

    are we home

--- global.js
+++ global.js
@@ -77,6 +77,8 @@ const pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
 for (let p of pages) {
     let url = p.url;
   
