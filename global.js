fb1f4d20ad19400c6cbf3dede2dd218f650ef229
ZeyuEdwardQi <zeqi@ucsd.edu>
2025-02-01 17:24:19 -0800

    Update global.js

--- global.js
+++ global.js
@@ -236,11 +236,11 @@ export async function fetchJSON(url) {
       }
 
       const data = await response.json();
-return data; 
+      return data; 
 
 
   } catch (error) {
-      console.error('Error fetching or parsing JSON data:', error);
+    console.error('Error fetching or parsing JSON data:', error);
   }
 }
 
