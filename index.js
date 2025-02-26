2e73570c0d1964616d657c2b70863611204e827d
ZeyuEdwardQi <zeqi@ucsd.edu>
2025-02-26 14:38:58 -0800

    format homepage

--- index.js
+++ index.js
@@ -1,54 +1,3 @@
-// import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
-
-// const projects = await fetchJSON('./lib/projects.json');
-// const latestProjects = projects.slice(0, 3);
-
-// const projectsContainer = document.querySelector('.projects');
-
-// <div class="projects"></div>
-
-// renderProjects(latestProjects, projectsContainer, 'h2');
-
-// const githubData = await fetchGitHubData('giorgianicolaou');
-
-// const profileStats = document.querySelector('#profile-stats');
-
-// if (profileStats) {
-//     profileStats.innerHTML = `
-//           <dl>
-//             <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-//             <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-//             <dt>Followers:</dt><dd>${githubData.followers}</dd>
-//             <dt>Following:</dt><dd>${githubData.following}</dd>
-//           </dl>
-//       `;
-// }
-
-// import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
-
-// // Load projects
-// const projects = await fetchJSON('./lib/projects.json');
-// const latestProjects = projects.slice(0, 3);
-// const projectsContainer = document.querySelector('.projects');
-// renderProjects(latestProjects, projectsContainer, 'h2');
-
-// // Load GitHub stats
-// const githubData = await fetchGitHubData('ZeyuEdwardQi'); // Use your GitHub username
-
-// const profileStats = document.querySelector('#profile-stats');
-
-// if (profileStats) {
-//     profileStats.innerHTML = `
-//           <h2>My GitHub Stats</h2>
-//           <dl>
-//             <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-//             <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-//             <dt>Followers:</dt><dd>${githubData.followers}</dd>
-//             <dt>Following:</dt><dd>${githubData.following}</dd>
-//           </dl>
-//       `;
-// }
-
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 const projectsContainer = document.querySelector('.projects');
 
@@ -88,36 +37,6 @@ async function loadProjects() {
         console.error("🚨 Error loading projects:", error);
     }
 }
-
-// Function to load GitHub stats
-// async function loadGitHubStats() {
-//     try {
-//         const username = "ZeyuEdwardQi"; // Change to your GitHub username
-//         const githubData = await fetchGitHubData(username);
-        
-//         if (!githubData) {
-//             console.error("⚠️ Error: GitHub data is undefined.");
-//             return;
-//         }
-
-//         const profileStats = document.querySelector('#profile-stats');
-//         if (profileStats) {
-//             profileStats.innerHTML = `
-//                 <h2>My GitHub Stats</h2>
-//                 <dl>
-//                     <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-//                     <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-//                     <dt>Followers:</dt><dd>${githubData.followers}</dd>
-//                     <dt>Following:</dt><dd>${githubData.following}</dd>
-//                 </dl>
-//             `;
-//         } else {
-//             console.error("⚠️ Error: #profile-stats container not found.");
-//         }
-//     } catch (error) {
-//         console.error("🚨 Error loading GitHub stats:", error);
-//     }
-// }
 
 const githubData = await fetchGitHubData('ZeyuEdwardQi');
 console.log("Github Data", githubData)
